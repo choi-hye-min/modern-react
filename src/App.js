84ad90e4c565f4ff44f86434e4c4919f39484901
arthur <gogoke1@naver.com>
2019-03-16 17:34:47 +0900

    add dev dependencies

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
-
-import Hello from './Hello';
-import ClassFunction from './ClassFunction';
+import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-          <Hello val={"arthur"}/>
-          <ClassFunction />
-      </React.Fragment>
-    );
-  }
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+        //https://yts.ag/api/v2/list_movies.json
+        console.log(this._tstFunction())
+    }
+
+    _tstFunction = () => {
+        return 111;
+    }
+
+    render() {
+
+        return (
+            <div>hello</div>
+        );
+    }
 }
 
 export default App;
