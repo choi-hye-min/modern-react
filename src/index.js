af7561e3fa66cd456f05a7a774d3acc904c01a8b
arthur <gogoke1@naver.com>
2019-03-18 01:55:34 +0900

    add extension store option

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {createStore} from "redux";
 import {initialState, taskReducer} from './reducers/taskReducer';
 import {Provider} from 'react-redux';
 
-const store = createStore(taskReducer);
+const store = createStore(taskReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 store.subscribe(() => {
     //console.log(store.getState());
